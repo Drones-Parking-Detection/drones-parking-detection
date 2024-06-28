369dc99f669ea9a2fc48e024fecf7fb73d9af964
Lea Margery <margerylea@gmail.com>
2024-06-28 23:54:56 +0200

    feat(kafka streams): separating topic drones-data in analysis-data and alert-data

---
+++ drone-kafka-stream/src/main/scala/KafkaStream.scala
@@ -0,0 +1,37 @@
+import drones.DroneData
+import drones.DroneSerializer.{fromJson, toJson}
+import org.apache.kafka.streams.{KafkaStreams, StreamsConfig}
+import org.apache.kafka.streams.scala.ImplicitConversions._
+import org.apache.kafka.streams.scala.StreamsBuilder
+import org.apache.kafka.streams.scala.serialization.Serdes
+import org.apache.kafka.streams.scala.Serdes._
+
+import java.time.LocalDateTime
+import java.util.Properties
+
+object KafkaStream extends App {
+  val streamsConfig = new Properties()
+  streamsConfig.put(StreamsConfig.APPLICATION_ID_CONFIG, "alerts-application")
+  streamsConfig.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
+  streamsConfig.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.intSerde.getClass)
+  streamsConfig.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.stringSerde.getClass)
+
+  val builder : StreamsBuilder = new StreamsBuilder()
+
+  val inputTopic : String = "drones-data"
+  val wronglyParkedTopic : String = "alert-data"
+  val analysisTopic : String = "analysis-data"
+
+  val inputStream = builder.stream[Int, String](inputTopic)
+
+  val alertProcessedStream = inputStream
+    .mapValues(x => fromJson(x).getOrElse(DroneData(-1, LocalDateTime.now(), (-1,-1), -1)))
+    .filter((_, data) => data.percentage < 50)
+    .mapValues(x => toJson(x))
+
+
+  alertProcessedStream.to(wronglyParkedTopic)
+  inputStream.to(analysisTopic)
+  val streams = new KafkaStreams(builder.build(),streamsConfig)
+  streams.start()
+}
