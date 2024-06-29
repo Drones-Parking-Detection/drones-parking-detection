322f9275368d5a9256f8b2e22d3d1fc75998e996
Dorian Penso <dorian.penso@epita.fr>
2024-06-29 10:43:51 +0200

    feat(statistic): create consumer to consume the kafka stream and push information in the data lake

--- statistic/SparkS3Example.scala
+++ statistic/SparkS3Example.scala
@@ -1,9 +1,15 @@
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{SparkSession, DataFrame}
+import org.apache.spark.sql.streaming.Trigger
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.functions._
+import java.sql.Timestamp
+
+case class DroneData(id: Int, time: Timestamp, coordinates: (Float, Float), percentage: Int)
 
 object SparkS3Example {
   def main(args: Array[String]): Unit = {
     val spark = SparkSession.builder()
-      .appName("Spark S3 Example")
+      .appName("KafkaToMinIO")
       .config("spark.hadoop.fs.s3a.endpoint", "http://s3:9000")
       .config("spark.hadoop.fs.s3a.access.key", "UserParking")
       .config("spark.hadoop.fs.s3a.secret.key", "PassWordParking")
@@ -13,17 +19,42 @@ object SparkS3Example {
 
     import spark.implicits._
 
-    println("Creating DataFrame...")
-    val data = Seq(("Alice", 29), ("Bob", 31), ("Cathy", 25))
-    val df = data.toDF("name", "age")
+    println("Consume data from Kafka...")
+
+    val kafkaStream = spark.readStream
+      .format("kafka")
+      .option("kafka.bootstrap.servers", "kafka-container:9092")
+      .option("subscribe", "drones-data")
+      .option("startingOffsets", "earliest")
+      .load()
+
+    // Define schema
+    val schema = StructType(Seq(
+      StructField("id", IntegerType),
+      StructField("time", TimestampType, nullable = false),
+      StructField("coordinates", StructType(Seq(
+        StructField("_1", FloatType),
+        StructField("_2", FloatType)
+      )), nullable = false),
+      StructField("percentage", IntegerType)
+    ))
+
+    // Convert JSON string to DataFrame
+    val droneData = kafkaStream.selectExpr("CAST(value AS STRING)").as[String]
+      .select(from_json($"value", schema).as("data"))
+      .select("data.*")
 
     println("Writing DataFrame to MinIO...")
-    df.write
-      .format("csv")
-      .save("s3a://my-bucket/my-data/")
 
-    println("Data written to MinIO successfully!")
-    spark.stop()
+    val query = droneData.writeStream
+      .outputMode("append")
+      .format("parquet")
+      .option("checkpointLocation", "/tmp/checkpoints")
+      .option("path", "s3a://my-bucket/streaming-data/")
+      .trigger(Trigger.ProcessingTime("20 seconds"))
+      .start()
+
+    query.awaitTermination()
   }
 }
 
--- statistic/build.sbt
+++ statistic/build.sbt
@@ -1,4 +1,4 @@
-name := "SparkS3Example"
+name := "KafkaToMinIO"
 
 version := "0.1"
 
@@ -7,6 +7,8 @@ scalaVersion := "2.12.10"
 libraryDependencies ++= Seq(
   "org.apache.spark" %% "spark-core" % "3.1.2",
   "org.apache.spark" %% "spark-sql" % "3.1.2",
-  "org.apache.hadoop" % "hadoop-aws" % "3.2.0"
+  "org.apache.spark" %% "spark-streaming" % "3.1.2",
+  "org.apache.spark" %% "spark-streaming-kafka-0-10" % "3.1.2",
+  "org.apache.hadoop" % "hadoop-aws" % "3.2.0",
+  "com.amazonaws" % "aws-java-sdk-bundle" % "1.11.874"
 )
-
